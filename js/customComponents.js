1990a2cc8df0fc5ab3578a10e640eae2d756d33d
Teresa Liang <teresaliang2003@gmail.com>
2023-12-29 14:46:05 -0800

    update information for third year

---
+++ js/customComponents.js
@@ -0,0 +1,101 @@
+class FlowerBulletPoint extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="bullet-point-container"><svg class="bullet-point"
+                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-flower3"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M11.424 8c.437-.052.811-.136 1.04-.268a2 2 0 0 0-2-3.464c-.229.132-.489.414-.752.767C9.886 4.63 10 4.264 10 4a2 2 0 1 0-4 0c0 .264.114.63.288 1.035-.263-.353-.523-.635-.752-.767a2 2 0 0 0-2 3.464c.229.132.603.216 1.04.268-.437.052-.811.136-1.04.268a2 2 0 1 0 2 3.464c.229-.132.489-.414.752-.767C6.114 11.37 6 11.736 6 12a2 2 0 1 0 4 0c0-.264-.114-.63-.288-1.035.263.353.523.635.752.767a2 2 0 1 0 2-3.464c-.229-.132-.603-.216-1.04-.268zM9 4a1.468 1.468 0 0 1-.045.205c-.039.132-.1.295-.183.484a12.88 12.88 0 0 1-.637 1.223L8 6.142a21.73 21.73 0 0 1-.135-.23 12.88 12.88 0 0 1-.637-1.223 4.216 4.216 0 0 1-.183-.484A1.473 1.473 0 0 1 7 4a1 1 0 1 1 2 0zM3.67 5.5a1 1 0 0 1 1.366-.366 1.472 1.472 0 0 1 .156.142c.094.1.204.233.326.4.245.333.502.747.742 1.163l.13.232a21.86 21.86 0 0 1-.265.002 12.88 12.88 0 0 1-1.379-.06 4.214 4.214 0 0 1-.51-.083 1.47 1.47 0 0 1-.2-.064A1 1 0 0 1 3.67 5.5zm1.366 5.366a1 1 0 0 1-1-1.732c.001 0 .016-.008.047-.02.037-.013.087-.028.153-.044.134-.032.305-.06.51-.083a12.88 12.88 0 0 1 1.379-.06c.09 0 .178 0 .266.002a21.82 21.82 0 0 1-.131.232c-.24.416-.497.83-.742 1.163a4.1 4.1 0 0 1-.327.4 1.483 1.483 0 0 1-.155.142zM9 12a1 1 0 0 1-2 0 1.476 1.476 0 0 1 .045-.206c.039-.131.1-.294.183-.483.166-.378.396-.808.637-1.223L8 9.858l.135.23c.241.415.47.845.637 1.223.083.19.144.352.183.484A1.338 1.338 0 0 1 9 12zm3.33-6.5a1 1 0 0 1-.366 1.366 1.478 1.478 0 0 1-.2.064c-.134.032-.305.06-.51.083-.412.045-.898.061-1.379.06-.09 0-.178 0-.266-.002l.131-.232c.24-.416.497-.83.742-1.163a4.1 4.1 0 0 1 .327-.4c.046-.05.085-.086.114-.11.026-.022.04-.03.041-.032a1 1 0 0 1 1.366.366zm-1.366 5.366a1.494 1.494 0 0 1-.155-.141 4.225 4.225 0 0 1-.327-.4A12.88 12.88 0 0 1 9.74 9.16a22 22 0 0 1-.13-.232l.265-.002c.48-.001.967.015 1.379.06.205.023.376.051.51.083.066.016.116.031.153.044l.048.02a1 1 0 1 1-1 1.732zM8 9a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+                </svg>
+            </div>
+        `;
+    }
+}
+
+class CourseItem extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        var currentStatus =  this.getAttribute("current-status");
+        if (currentStatus == "true"){
+            var currentCourseStyling = "class='current-course'"
+        } else {
+            var currentCourseStyling = ""
+        }
+
+        this.innerHTML = `
+            <li>
+                <flower-bullet></flower-bullet>
+                <div ${currentCourseStyling}>${this.getAttribute("course-name")}</div>
+            </li>
+        `;
+    }
+}
+
+class ProjectCard extends HTMLElement {
+    constructor() {
+        super();
+    }
+    
+    connectedCallback() {
+        var websiteLink = this.getAttribute("website-link");
+        var languages = this.getAttribute("languages").split(",");
+        this.innerHTML = `
+            <li>
+                <a target="_blank" href="${this.getAttribute("github-link")}">
+                    <div class="project-card">
+                        <div class="project-card-header">
+                            <svg class="MuiSvgIcon-root" focusable="false" fill="currentColor"
+                                viewBox="0 0 24 24">
+                                <path
+                                    d="M20 6h-8l-1.41-1.41C10.21 4.21 9.7 4 9.17 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-1 12H5c-.55 0-1-.45-1-1V9c0-.55.45-1 1-1h14c.55 0 1 .45 1 1v8c0 .55-.45 1-1 1z">
+                                </path>
+                            </svg>
+                            <div class="external-links">
+                                <a target="_blank"
+                                    href="${this.getAttribute("github-link")}"><svg
+                                        xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round"
+                                        class="feather feather-github">
+                                        <title>GitHub</title>
+                                        <path
+                                            d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
+                                        </path>
+                                    </svg></a>
+                                ${websiteLink != null? `<a target="_blank" href="${websiteLink}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                        class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd"
+                                            d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z" />
+                                        <path fill-rule="evenodd"
+                                            d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z" />
+                                    </svg>
+                                </a>` : ""}
+                            </div>
+                        </div>
+                        <div class="project-card-title">
+                            ${this.getAttribute("project-name")}
+                        </div>
+                        <div class="project-card-description">
+                            ${this.getAttribute("project-description")}
+                        </div>
+                        <div class="project-card-footer">
+                            ${languages.map(language => `<div class="language">${language}</div>`).join("")}
+                        </div>
+                    </div>
+                </a>
+            </li>
+        `;
+    }
+}
+
+customElements.define("flower-bullet", FlowerBulletPoint);
+customElements.define("course-item", CourseItem);
+customElements.define("project-card", ProjectCard);
